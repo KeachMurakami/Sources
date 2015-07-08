86514a64058a0afd31c2f5a854c48a88fb3710f6
MurakamiKeach <m.keach.11@gmail.com>
2015-07-08 12:04:22 +0900

    微調整

--- standard_funs.R
+++ standard_funs.R
@@ -4,15 +4,26 @@ se <- function(x) sd(x, na.rm = TRUE)/sqrt(sum(!is.na(x)))
 length2 <- function(x) sum(!is.na(x))
 
 library(shiny)
+
+# web scraping
+library(RCurl)
+library(googlesheets)
+library(rdrop2)
+
+# data handlings
 library(plyr)
 library(dplyr)
-library(ggplot2)
 library(data.table)
 library(stringr)
 library(magrittr)
 library(tidyr)
 library(reshape2)
-library(RCurl)
-library(agricolae)
+
+
+# visualize
+library(ggplot2)
 library(rCharts)
 library(googleVis)
+
+# statistical
+library(agricolae)
