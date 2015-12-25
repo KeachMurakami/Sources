b1c34269eac057b73155d809e8d330a61ba5d351
MurakamiKeach <m.keach.11@gmail.com>
2015-12-25 21:41:05 +0900

    Minor

--- Startup.R
+++ Startup.R
@@ -1,25 +1,5 @@
 temp.dir <- getwd()
 
-if(.Platform$OS.type == "unix"){
-  # startups for Mac
-  system("defaults write org.R-project.R force.LANG en_US.UTF-8")
-  # in order to avoid warning message
-  # http://stackoverflow.com/questions/9689104/installing-r-on-mac-warning-messages-setting-lc-ctype-failed-using-c
-  home.dir <- function() setwd("/Users/keach/Dropbox/R")
-  library(slackr)
-  slackr_setup(config_file = ".slackr")
-  setwd(temp.dir)
-} else {
-  # startups for Win
-  home.dir <- function() setwd("C:/Users/KeachMurakami/Dropbox/R")
-}
-home.dir()
-
-options(help_type="html")
-options(warn=1)
-
-# set working directory "/dropbox/R"
-
 library(MASS)
 library(ggplot2) #ggplot2
 library(RColorBrewer) 
@@ -41,9 +21,39 @@ library(data.table)
 library(lubridate)
 library(scales)
 library(pforeach)
+library(devtools)
 
-source("./Sources/functions.R")
-source("./Sources/labels.R")
+OFFline <- 
+  class(try(source("http://www7b.biglobe.ne.jp/~homunculus/Rsource/tameshi.txt"), silent = T)) == "try-error"
+  #class(try(source_url(url = "https://raw.githubusercontent.com/KeachMurakami/Sources/master/NetworkTest.txt")), silent = T)) == "try-error"
 
-setwd(temp.dir)
-rm(temp.dir)
+if(.Platform$OS.type == "unix"){
+  # startups for Mac
+  system("defaults write org.R-project.R force.LANG en_US.UTF-8")
+  # in order to avoid warning message
+  # http://stackoverflow.com/questions/9689104/installing-r-on-mac-warning-messages-setting-lc-ctype-failed-using-c
+  home.dir <- function() setwd("/Users/keach/Dropbox/R")
+  library(slackr)
+  slackr_setup(config_file = ".slackr")
+  setwd(temp.dir)
+} else {
+  # startups for Win
+  home.dir <- function() setwd("C:/Users/KeachMurakami/Dropbox/R")
+}
+home.dir()
+}
+
+options(help_type="html")
+options(warn=1)
+
+# set working directory "/dropbox/R"
+
+if(OFFline){
+  source("./Sources/functions.R")
+  source("./Sources/labels.R")
+  cat("#####warning######\nThe functions are read from local files\nIt might not be the latest ver.")  
+} else {
+  library(RCurl)
+  eval(parse(text = getURL("https://raw.githubusercontent.com/KeachMurakami/Sources/master/functions.R", ssl.verifypeer = FALSE)))
+  eval(parse(text = getURL("https://raw.githubusercontent.com/KeachMurakami/Sources/master/labels.R", ssl.verifypeer = FALSE)))
+}
