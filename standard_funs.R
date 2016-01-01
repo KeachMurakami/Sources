f9be65b4a9448258ee31ead3408aa27ae1926e1d
MurakamiKeach <m.keach.11@gmail.com>
2016-01-01 16:48:35 +0900

    Minor
    melt2: reshape2::meltのid.varsでの文字列指定をカラム番号で指定する糖衣構文。

--- standard_funs.R
+++ standard_funs.R
@@ -1,4 +1,24 @@
-mean2 <- function(x) mean(x, na.rm = T)
-sd2 <- function(x) sd(x, na.rm = TRUE)
-se <- function(x) sd(x, na.rm = TRUE)/sqrt(sum(!is.na(x)))
-length2 <- function(x) sum(!is.na(x))
+mean2 <- 
+  function(x) mean(x, na.rm = T)
+
+sd2 <- 
+  function(x) sd(x, na.rm = TRUE)
+
+se <- 
+  function(x) sd(x, na.rm = TRUE) / sqrt(sum(!is.na(x)))
+
+length2 <- 
+  function(x) sum(!is.na(x))
+
+melt2 <- 
+  function(df, cols, ...) {
+    # syntax sugar for melt
+    # input numerics to id.vars
+    
+    if(length(cols) >= dim(df)[2]) stop(message = "undefined columns selected")
+
+    cols %>%
+    as.numeric %>%
+    colnames(df)[.] %>%
+    reshape2::melt(df, id.vars = ., ...)
+  }
