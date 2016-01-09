9d90ec720eacafeadb551a1171a3db68d9037835
MurakamiKeach <m.keach.11@gmail.com>
2016-01-09 19:34:16 +0900

    Major
    ggplotの連続値の軸のコントロール。
    expand = c(0,0) の糖衣構文的なやつ。

--- theme_ggplot.R
+++ theme_ggplot.R
@@ -11,3 +11,20 @@ gg_theme <- function (legend = "none", ...) {
           panel.margin = unit(0, units = "points")
     )
 }
+
+### fix axes ranges
+gg_xy <-
+  function(ranges, ...){
+    list(gg_x(ranges[1:2], ...),
+         gg_y(ranges[3:4], ...))
+  }
+
+gg_x <-
+  function(x_range, ...){
+    scale_x_continuous(limits = range(x_range), expand = c(0, 0), ...)
+  }
+
+gg_y <-
+  function(y_range, ...){
+    scale_y_continuous(limits = range(y_range), expand = c(0, 0), ...)
+  }
