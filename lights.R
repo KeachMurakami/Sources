81e1df082235ca757f2b601b8c439cb3cc812192
MurakamiKeach <m.keach.11@gmail.com>
2016-01-18 21:08:52 +0900

    Minor
    分光分布作成関数の追加。

--- lights.R
+++ lights.R
@@ -1,36 +1,61 @@
+if(OFFline){
+  tmp_dir <- getwd(); home.dir()
+  phy.equ <-
+    read.csv("~/Dropbox/R/Sources/data/phytochrome.csv")
+  setwd(tmp_dir)
+} else {
+  txt <-
+    getURL("https://raw.githubusercontent.com/KeachMurakami/Sources/master/data/phytochrome.csv", ssl.verifypeer = FALSE)
+  write(txt, smp <- tempfile()) # write the Sager's data to temporary file
+  phy.equ <- read.csv(smp)
+}
+
+
 PPS <-
   function(df, digits = 3){
-  lapply(1:(dim(df)[2] - 1), function(X){
-    df <- merge(phy.equ, df, by = "wavelength")
-    sig.r <- sum(df[, (X + 3)] * df[, 2], na.rm = TRUE)
-    sig.fr <- sum(df[, (X + 3)] * df[, 3], na.rm = TRUE)
-    equ <- sig.r / (sig.r + sig.fr)
-    return(equ)
-  }) %>%
-    unlist() %>%
-    round(digits = digits)  
-}
+    colnames(df)[1] <- "wavelength"
+    lapply(1:(dim(df)[2] - 1), function(X){
+      df <- merge(phy.equ, df, by = "wavelength")
+      sig.r <- sum(df[, (X + 3)] * df[, 2], na.rm = TRUE)
+      sig.fr <- sum(df[, (X + 3)] * df[, 3], na.rm = TRUE)
+      equ <- sig.r / (sig.r + sig.fr)
+      return(equ)
+    }) %>%
+      unlist() %>%
+      round(digits = digits)  
+  }
 
 PFD <-
   function(df, from = 350, to = 800, digits = 1){
-  lapply(1:(dim(df)[2] - 1), function(X){
-    df %>%
-      filter(wavelength >= from, wavelength <= to) %>%
-      .[, X + 1] %>%
-      sum() / 1000
-  }) %>%
-  unlist() %>%
-  round(digits = digits)
-}
+    colnames(df)[1] <- "wavelength"
+    lapply(1:(dim(df)[2] - 1), function(X){
+      df %>%
+        filter(between(wavelength, from, to)) %>%
+        .[, X + 1] %>%
+        sum() / 1000
+    }) %>%
+      unlist() %>%
+      round(digits = digits)
+  }
 
 PPFD <- function(df, digits = 1) PFD(df, 400, 700, digits)
 
-LIGHT <- function(df, digits = c(1,1,3)){
-  colnames(df)[1] <- "wavelength"
-  rbind(PPFD(df, digits[1]),
-        PFD(df, digits = digits[2]),
-        PPS(df, digits = digits[3])
-        ) %>%
-    as.data.frame %>%
-    cbind(label = c("PPFD", "PFD", "PPS"), .)
-}
+LIGHT <-
+  function(df, digits = c(1,1,3)){
+    colnames(df)[1] <- "wavelength"
+    rbind(PPFD(df, digits[1]),
+          PFD(df, digits = digits[2]),
+          PPS(df, digits = digits[3])
+    ) %>%
+      as.data.frame %>%
+      cbind(label = c("PPFD", "PFD", "PPS"), .)
+  }
+
+SPD <-
+  function(df, from = 350, to = 800){
+    colnames(df)[1] <- "wavelength"
+    df %>%
+      melt2(1) %>%
+      ggplot(aes(x = wavelength, y = value, col = variable)) +
+      geom_line()
+  }
