a044e5d7d5aa8531b536680f4ae82c991b0ee43b
MurakamiKeach <m.keach.11@gmail.com>
2016-06-17 20:12:29 +0900

    Add my theme
    theme_bw2 modification in tick colour, legend

--- ggplot_accessory.R
+++ ggplot_accessory.R
@@ -1,18 +1,28 @@
 ### for my ggplot2 theme
-gg_theme <- function (legend = "none", ...) {
-  theme_bw(...) %+replace%
-    theme(panel.grid.minor = element_blank(),
-          panel.grid.major = element_blank(),
-          legend.position = legend,
-          legend.key = element_blank(),
-          legend.background = element_blank(),
-          panel.border = element_rect(fill = NA,colour="black"),  
-          strip.background = element_blank(),
-          plot.background = element_blank(),
-          panel.background = element_blank(),
-          panel.margin = unit(0, units = "points")
+
+theme_bw2 <-
+  function(base_size = 12, base_family = "", legend = "none"){
+    theme_bw(...) %+replace%
+    theme(legend.position = legend,
+          axis.ticks = element_line(colour = "grey50")
     )
-}
+  }
+
+gg_theme <-
+  function (base_size = 12, base_family = "", legend = "none", ...) {
+    theme_bw(...) %+replace%
+      theme(panel.grid.minor = element_blank(),
+            panel.grid.major = element_blank(),
+            legend.position = legend,
+            legend.key = element_blank(),
+            legend.background = element_blank(),
+            panel.border = element_rect(fill = NA,colour="black"),  
+            strip.background = element_blank(),
+            plot.background = element_blank(),
+            panel.background = element_blank(),
+            panel.margin = unit(0, units = "points")
+      )
+  }
 
 ### fix axes ranges
 gg_xy <-
