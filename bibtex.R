750b6c97ff50a463bd5870d615eb9f0a1b158b1b
MurakamiKeach <m.keach.11@gmail.com>
2016-07-27 18:14:39 +0900

    Fix minor

--- bibtex.R
+++ bibtex.R
@@ -4,15 +4,14 @@ bib_update <-
     if(OS != "unix"){
       stop("Win用にパスを設定していないのでMacでやる")
     } else {
-      paste0("cat ", input_dir, "*.bib > ", output_dir, "full_with.bib") %>%
-        system
-      paste0("cat ", input_dir, "*.bib > ", output_dir, "full_without.bib") %>%
-        system
+      paste0("cat ", input_dir, "*.bib > ", output_dir, "full_with.bib") %>% system
+      paste0("cat ", output_dir, "*with.bib > ", output_dir, "with_period.bib") %>% system
+
+      paste0("mv ", output_dir, "full_with.bib ", output_dir, "full_without.bib") %>% system
+      paste0("cat ", output_dir, "*without.bib > ", output_dir, "without_period.bib") %>% system
       
-      paste0("cat ", output_dir, "*with.bib > ", output_dir, "with_period.bib") %>%
-        system
-      paste0("cat ", output_dir, "*without.bib > ", output_dir, "without_period.bib") %>%
-        system
+      paste0("mv ", output_dir, "full_without.bib ", output_dir, "full.bib") %>% system
+      
     }
     cat("\ncitations have been updated.\n\n")
   }
