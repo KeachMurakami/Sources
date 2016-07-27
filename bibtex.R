e8b42a89df3d0d49ac9da4845db7fc7c19b5ded5
MurakamiKeach <m.keach.11@gmail.com>
2016-07-27 18:35:39 +0900

    Add function
    省略名リストをアップデートする関数を追加。

--- bibtex.R
+++ bibtex.R
@@ -4,14 +4,33 @@ bib_update <-
     if(OS != "unix"){
       stop("Win用にパスを設定していないのでMacでやる")
     } else {
+      
       paste0("cat ", input_dir, "*.bib > ", output_dir, "full_with.bib") %>% system
       paste0("cat ", output_dir, "*with.bib > ", output_dir, "with.bib") %>% system
-
+      
       paste0("mv ", output_dir, "full_with.bib ", output_dir, "full_without.bib") %>% system
       paste0("cat ", output_dir, "*without.bib > ", output_dir, "without.bib") %>% system
       
       paste0("mv ", output_dir, "full_without.bib ", output_dir, "full.bib") %>% system
       
     }
-    cat("\ncitations have been updated.\n\n")
+    cat("\nCitations have been updated.\n\n")
+  }
+
+abbrv_update <-
+  function(abbrv_file = "~/GitHub/BeeLabR/BibTex/MyList.xlsx"){
+    abbrv_list <-
+      xlsx::read.xlsx(abbrv_file, sheetIndex = 1)
+    abbrv_dir <-
+      dirname(abbrv_file)
+    
+    abbrv_list$Abbrv_with_period.1 %>%
+      as.character %>%
+      write(file = paste0(abbrv_dir, "/abbrv_with.bib"))
+    
+    abbrv_list$Abbrv_without_period.1 %>%
+      as.character %>%
+      write(file = paste0(abbrv_dir, "/abbrv_without.bib"))
+    
+    cat("\nAbbreviations have been updated.\n\n")
   }
