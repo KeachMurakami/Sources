a35146dfda0f9407751b5438c79e9e5b9bcec675
MurakamiKeach <m.keach.11@gmail.com>
2016-08-05 14:31:02 +0900

    Add minor
    Supへの対応。
    Closureを使いたいが勉強しないと。

--- TeX_accessory.R
+++ TeX_accessory.R
@@ -6,14 +6,36 @@ EqnNum <-
     return(paste0(head, EqnNum_count))
   }
 
+EqnSupNum <- 
+  function(head = EqnSupHead){
+    EqnSupNum_count <<- EqnSupNum_count + 1
+    return(paste0(head, EqnSupNum_count))
+  }
+
+
+
 FigNum <- 
   function(Caption, head = FigHead){
     FigNum_count <<- FigNum_count + 1
     return(paste0(head, FigNum_count, " ", Caption))
   }
 
+FigSupNum <- 
+  function(Caption, head = FigSupHead){
+    FigSupNum_count <<- FigSupNum_count + 1
+    return(paste0(head, FigSupNum_count, " ", Caption))
+  }
+
+
+
 TableNum <- 
   function(Caption, head = TableHead){
     TableNum_count <<- TableNum_count + 1
     return(paste0(head, TableNum_count, " ", Caption))
+  }
+
+TableSupNum <- 
+  function(Caption, head = TableSupHead){
+    TableSupNum_count <<- TableSupNum_count + 1
+    return(paste0(head, TableSupNum_count, " ", Caption))
   }
