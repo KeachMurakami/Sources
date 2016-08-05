b9f456da265cfb6a9063b7ca62395cca03c941b3
MurakamiKeach <m.keach.11@gmail.com>
2016-08-05 13:19:15 +0900

    Add functions
    Eon, Fig, Tableにオートで番号を。

--- TeX_accessory.R
+++ TeX_accessory.R
@@ -1,7 +1,19 @@
-### add Eqn number automatically
+### add Eqn, Fig, Table number automatically
 
 EqnNum <- 
   function(head = EqnHead){
     EqnNum_count <<- EqnNum_count + 1
     return(paste0(head, EqnNum_count))
+  }
+
+FigNum <- 
+  function(Caption, head = FigHead){
+    FigNum_count <<- FigNum_count + 1
+    return(paste0(head, FigNum_count, Caption))
+  }
+
+TableNum <- 
+  function(Caption, head = TableHead){
+    TableNum_count <<- TableNum_count + 1
+    return(paste0(head, TableNum_count, Caption))
   }
