b83013c08191ce1f9e93586ed043bbea093e49a6
MurakamiKeach <m.keach.11@gmail.com>
2016-08-12 19:13:26 +0900

    Fix minor
    執筆ログにコメント機能

--- MyData/WriteLog.R
+++ MyData/WriteLog.R
@@ -10,34 +10,32 @@ require(lubridate)
 require(tidyr)
 
 WriteLog <- function(PubName = "", LogFile = "~/Dropbox/R/my.data/MyWrite.csv"){
-  if(PubName == ""){
+  input <-
     fread(LogFile) %>%
     gather(Pub, words, -Date) %>%
     na.omit %>%
-    mutate(Date = mdy(Date)) %>%
-    ggplot(aes(Date, words, group = Pub, col = Pub)) +
-    theme_bw() +
-    theme(legend.position = c(.8, .4),
-          legend.key = element_blank(),
-          legend.title = element_blank(),
-          legend.background = element_blank()) +
-    geom_line() +
-    geom_point() +
-    ylab("Words in the body")
-  } else {
-  fread(LogFile) %>%
-    gather(Pub, words, -Date) %>%
-    na.omit %>%
-    mutate(Date = mdy(Date)) %>%
+    separate(col = words, into = c("words", "comment"), sep = "_") %>%
+    mutate(Date = mdy(Date),
+           words = as.numeric(words))
+
+    Task_all <- 
+      input$Pub %>%
+      unique %>%
+      as.character
+      
+    PubName <-
+      Vectorize(ifelse)(PubName == "", Task_all, PubName)
+  
+  input %>%  
     filter(Pub %in% PubName) %>%
-    ggplot(aes(Date, words, group = Pub, col = Pub)) +
+    ggplot(aes(Date, words, group = Pub, col = Pub, label = comment)) +
     theme_bw() +
-    theme(legend.position = c(.8, .4),
+    theme(legend.position = "none",
           legend.key = element_blank(),
           legend.title = element_blank(),
           legend.background = element_blank()) +
     geom_line() +
     geom_point() +
+    geom_text(aes(y = as.numeric(words) + 100)) +
     ylab("Words in the body")
   }
-}
